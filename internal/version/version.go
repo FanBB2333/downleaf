7ea7b5a3738c9bc5a801ce2dd37b296410e2d4eb
L1ght <2504082957@qq.com>
2026-04-20 12:02:21 +0800

    chore(release): bump version to v0.3.5

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the single source of truth for the application version.
 // Update this value when releasing a new version.
-const Version = "v0.3.4-r1"
+const Version = "v0.3.5"
